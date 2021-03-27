71abae5efda659d2f9ea682fa9b81b336b706cff
PauliusU <48020370+PauliusU@users.noreply.github.com>
2021-03-27 00:51:19 -0700

    Initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt upgrade
+
+# ##############################################################################
+# PROGRAMMING TOOLS AND RUNTIMES
+# ##############################################################################
+sudo apt install -y git
+sudo apt install -y nodejs
+sudo snap install --classic code # VSCode
+sudo apt install -y python-is-python3
+
+# ##############################################################################
+# OTHER PROGRAMS
+# ##############################################################################
+sudo apt install -y rclone # Dropbox, Google Drive and other cloud services
+sudo apt install -y qbittorrent
