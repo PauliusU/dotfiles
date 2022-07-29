837f3da89be80f183f8e8cdc5cd1adb2489377f0
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-07-29 17:57:58 +0300

    style: unify hs keyboard shortcut descriptions

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -5,22 +5,22 @@ hs.alert.show("Config loaded")
 ---------------------------- Keyboard shortcuts --------------------------------
 
 -- HammerSpoon settings
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", fn.reloadHs()) -- Reload HS config #1
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Y", function() hs.reload() end) -- Reload HS config #2 (inline example)
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", fn.sayHello)
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", fn.reloadHs()) -- Reload HS config #1 [ ⌃ + ⌥ + ⌘ + R ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Y", function() hs.reload() end) -- Reload HS config #2 (inline example) [ ⌃ + ⌥ + ⌘ + Y ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", fn.sayHello) -- [ ⌃ + ⌥ + ⌘ + W ]
 
 -- Text manipulation
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "D", fn.getDate)
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "D", fn.getDate) -- [ ⌃ + ⌥ + ⌘ + D ]
 
 -- Quick open applications and paths
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "C", fn.open("Google Chrome"))
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "T", fn.open("Terminal"))
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "V", fn.open("Visual Studio Code"))
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", fn.open("Finder"))
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "E", fn.exec("open ~")) -- Home folder
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "C", fn.open("Google Chrome")) -- [ ⌃ + ⌥ + ⌘ + C ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "T", fn.open("Terminal")) -- [ ⌃ + ⌥ + ⌘ + T ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "V", fn.open("Visual Studio Code")) -- [ ⌃ + ⌥ + ⌘ + V ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "F", fn.open("Finder")) -- [ ⌃ + ⌥ + ⌘ + F ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "E", fn.exec("open ~")) -- Home folder [ ⌃ + ⌥ + ⌘ + E ]
 
 -- System control (windows management, sound)
 hs.hotkey.bind({ "alt" }, "F4", fn.closeWindow()) -- [ ⌥ + F4 ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "H", fn.moveLeft())
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "F11", fn.volumeChange(-10)) -- Decrease volume
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "F12", fn.volumeChange(10)) -- Increase volume
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "H", fn.moveLeft()) -- [ ⌃ + ⌥ + ⌘ + H ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "F11", fn.volumeChange(-10)) -- Decrease volume [ ⌃ + ⌥ + ⌘ + F11 ]
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "F12", fn.volumeChange(10)) -- Increase volume [ ⌃ + ⌥ + ⌘ + F12 ]
