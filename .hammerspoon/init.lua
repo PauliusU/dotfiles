383aad5840d3bf7aefe12718ef0bf95b530306c8
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-08-20 13:49:29 +0300

    style: unify order of modifier keys in hs

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -6,24 +6,28 @@ hs.alert.show("Config loaded")
 ---------------------------- Keyboard shortcuts --------------------------------
 
 -- HammerSpoon settings
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", fn.reloadHs()) -- Reload HS config #1 [ ⌃ + ⌥ + ⌘ + R ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "Y", function() hs.reload() end) -- Reload HS config #2 (inline example) [ ⌃ + ⌥ + ⌘ + Y ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", fn.sayHello) -- [ ⌃ + ⌥ + ⌘ + W ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "R", fn.reloadHs()) -- Reload HS config #1 [ ⌃ + ⌥ + ⌘ + R ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "Y", function() hs.reload() end) -- Reload HS config #2 (inline example) [ ⌃ + ⌥ + ⌘ + Y ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "W", fn.sayHello) -- [ ⌃ + ⌥ + ⌘ + W ]
 
 -- Text manipulation
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "D", fn.getDate) -- [ ⌃ + ⌥ + ⌘ + D ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "D", fn.getDate) -- [ ⌃ + ⌥ + ⌘ + D ]
 
 -- Quick open applications and paths
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "C", fn.open("Google Chrome")) -- [ ⌃ + ⌥ + ⌘ + C ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "T", fn.open("Terminal")) -- [ ⌃ + ⌥ + ⌘ + T ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "V", fn.open("Visual Studio Code")) -- [ ⌃ + ⌥ + ⌘ + V ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "F", fn.open("Finder")) -- [ ⌃ + ⌥ + ⌘ + F ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "E", fn.exec("open ~")) -- Home folder [ ⌃ + ⌥ + ⌘ + E ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "C", fn.open("Google Chrome")) -- [ ⌃ + ⌥ + ⌘ + C ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "T", fn.open("Terminal")) -- [ ⌃ + ⌥ + ⌘ + T ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "V", fn.open("Visual Studio Code")) -- [ ⌃ + ⌥ + ⌘ + V ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "F", fn.open("Finder")) -- [ ⌃ + ⌥ + ⌘ + F ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "E", fn.exec("open ~/Downloads")) -- Home folder [ ⌃ + ⌥ + ⌘ + E ]
 
 -- System control (windows management, sound)
 hs.hotkey.bind({ "ctrl" }, "space", fn.toggleLayout()) -- Overrides system layout chooser [ ⌃ + Space ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "up", fn.maximizeWindow()) -- [ ⌃ + ⌥ + ⌘ + Up ]
-hs.hotkey.bind({ "cmd", "alt", "ctrl", "shift"}, "R", fn.restartOSWindow()) -- [ ⌃ + ⌥ + ⌘ + ⇧ + R ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "up", fn.maximizeWindow()) -- [ ⌃ + ⌥ + ⌘ + Up ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd", "shift"}, "R", fn.restartOSWindow()) -- [ ⌃ + ⌥ + ⌘ + ⇧ + R ]
 hs.hotkey.bind({ "alt" }, "F4", fn.closeWindow()) -- [ ⌥ + F4 ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "H", fn.hideAllWindows()) -- [ ⌃ + ⌥ + ⌘ + H ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "J", fn.moveLeft()) -- [ ⌃ + ⌥ + ⌘ + J ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "F11", fn.volumeChange(-10)) -- Decrease volume [ ⌃ + ⌥ + ⌘ + F11 ]
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "F12", fn.volumeChange(10)) -- Increase volume [ ⌃ + ⌥ + ⌘ + F12 ]
 hs.hotkey.bind({ "alt", "cmd" }, "up", fn.volumeChange(-10)) -- Decrease volume [ ⌥ + ⌘ + ↑ ]
 hs.hotkey.bind({ "alt", "cmd" }, "down", fn.volumeChange(10)) -- Increase volume [ ⌥ + ⌘ + ↓ ]
