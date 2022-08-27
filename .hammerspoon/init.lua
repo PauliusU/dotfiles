b05b552e33481a00eb9208e2c8d47c23d26019a7
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-08-28 00:45:31 +0300

    feat: add MPV and Ferdi shortcuts

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -32,11 +32,11 @@ hs.hotkey.bind(mash, "D", fn.getDate) -- [ ⌃ + ⌥ + ⌘ + D ]
 -- VIM like arrow keys
 hs.hotkey.bind(mash, "I", fn.press({}, "up")) -- ↑
 hs.hotkey.bind(mash, "K", fn.press({}, "down")) -- ↓
-hs.hotkey.bind(mash, "J", fn.press({}, "left")) -- ←
+hs.hotkey.bind(mash, "J", fn.press({}, "left")) -- ← ◀
 hs.hotkey.bind(mash, "L", fn.press({}, "right")) -- →
 -- NON VIM like shortcuts
 hs.hotkey.bind(mash, "U", fn.press({ "alt" }, "left")) -- left one word
-hs.hotkey.bind(mash, "O", fn.press({ "alt" }, "end")) -- one word right
+hs.hotkey.bind(mash, "O", fn.press({ "alt" }, "right")) -- one word right
 hs.hotkey.bind(mash, "H", fn.press({}, "home")) -- start of the line
 hs.hotkey.bind(mash, ";", fn.press({}, "end")) -- end of the line
 -- Programming keys in any keyboard layout
@@ -63,22 +63,26 @@ hs.hotkey.bind({"alt"}, 29, fn.printText("0")) -- 0
 
 -- Quick open applications, files and paths
 Hyper:bind({}, "V", nil, fn.open("Google Chrome"))
-Hyper:bind({}, "B", nil, fn.open("MongoDB Compass"))
 Hyper:bind({}, "G", nil, fn.exec("open 'https://gmail.com'"))
 Hyper:bind({}, "L", nil, fn.exec("open 'https://calendar.google.com'"))
 hs.hotkey.bind(mash, "V", fn.open("Google Chrome")) -- [ ⌃ + ⌥ + ⌘ + C ]
-Hyper:bind({}, "T", nil, fn.open("Terminal"))
-hs.hotkey.bind(mash, "T", fn.open("Terminal")) -- [ ⌃ + ⌥ + ⌘ + T ]
 hs.hotkey.bind("ctrl", "escape", fn.openTab()) -- Open Google Chrome tab [ ⌃ + ⎋ Escape (Esc) ]
 hs.hotkey.bind("alt", "escape", fn.openTab()) -- Open Google Chrome tab [ ⌥ + ⎋ Escape (Esc) ]
+Hyper:bind({}, "A", nil, fn.open("Terminal"))
+Hyper:bind({}, "T", nil, fn.open("Terminal"))
+hs.hotkey.bind(mash, "T", fn.open("Terminal")) -- [ ⌃ + ⌥ + ⌘ + T ]
 hs.hotkey.bind(ctrl_opt, "Z", fn.open("Terminal")) -- [ ⌃ + ⌥ + Z ]
-Hyper:bind({}, "C", nil, fn.open("Visual Studio Code"))
+Hyper:bind({}, "X", nil, fn.open("Visual Studio Code"))
 hs.hotkey.bind(mash, "C", fn.open("Visual Studio Code")) -- [ ⌃ + ⌥ + ⌘ + V ]
+Hyper:bind({}, "B", nil, fn.open("MongoDB Compass"))
+Hyper:bind({}, "P", nil, fn.open("MPV"))
 hs.hotkey.bind(mash, "F", fn.open("Finder")) -- Finder (recent files) [ ⌃ + ⌥ + ⌘ + F ]
-Hyper:bind({}, "A", fn.open("Marta"))
+Hyper:bind({}, "S", fn.open("Marta"))
 hs.hotkey.bind(mash, "E", fn.exec("open ~/Downloads")) -- Downloads folder [ ⌃ + ⌥ + ⌘ + E ]
 hs.hotkey.bind(shift_mash, "F5", fn.exec("open ~/Dropbox/DropsyncFiles/audiob/notes_PC.md")) -- notes_PC [ ⌃ + ⌥ + ⇧ + ⌘ + F5 ]
-Hyper:bind({}, "S", nil, fn.open("Slack"))
+Hyper:bind({}, "N", nil, fn.exec("open ~/Dropbox/DropsyncFiles/audiob/notes_PC.md"))
+Hyper:bind({}, "L", nil, fn.open("Slack"))
+Hyper:bind({}, "K", nil, fn.open("Ferdi"))
 
 -- Window management
 -- feature spectacle/another window sizing apps
@@ -99,10 +103,11 @@ Hyper:bind({}, "return", nil, fn.maximizeWindow()) -- [ Hyper + ↩ ]
 hs.hotkey.bind(mash, "up", fn.maximizeWindow()) -- [ ⌃ + ⌥ + ⌘ + Up ]
 Hyper:bind({}, "F", nil, fn.toggleFullscreen()) -- [ Hyper + F ]
 hs.hotkey.bind("alt", "F4", fn.closeWindow()) -- [ ⌥ + F4 ]
+Hyper:bind({}, "M", nil, fn.hideAllWindows()) -- [ Hyper + M ]
 hs.hotkey.bind(mash, "M", fn.hideAllWindows()) -- [ ⌃ + ⌥ + ⌘ + H ]
 hs.hotkey.bind(mash, "G", fn.moveLeft()) -- [ ⌃ + ⌥ + ⌘ + J ]
-Hyper:bind({ "cmd" }, "left", fn.prevMonitor())
-Hyper:bind({ "cmd" }, "right", fn.nextMonitor())
+Hyper:bind({ "cmd" }, "left", fn.prevMonitor()) -- [Hyper + ⌘ + ◀ ]
+Hyper:bind({ "cmd" }, "right", fn.nextMonitor()) -- [Hyper + ⌘ + ▶ ]
 
 -- System control (sound, keyboar layout)
 hs.hotkey.bind(shift_mash, "R", fn.restartOSWindow()) -- [ ⌃ + ⌥ + ⌘ + ⇧ + R ]
