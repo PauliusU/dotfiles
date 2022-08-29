5f2cc1703a6c544cf146ac54048bc899a50eeea3
Paulius Uosis <48020370+PauliusU@users.noreply.github.com>
2022-08-29 14:42:45 +0300

    feat: add file associations

---
+++ file-associations.zsh
@@ -0,0 +1,15 @@
+# Get the default app id for .sh and .jpg files (without a dot)
+duti -x sh
+duti -x jpg
+
+# TEXT FILES com.microsoft.VSCode
+duti -s com.microsoft.VSCode .md all
+duti -s com.microsoft.VSCode .sh all
+
+# AUDIO io.mpv
+duti -s io.mpv .m4a all
+duti -s io.mpv .opus all
+
+# VIDEO io.mpv
+
+# IMAGES
