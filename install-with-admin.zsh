14ca36a39ec99b0da4795dc277cb4845d6f38b58
Paulius Uosis <paulius.uosis@vialet.eu>
2022-08-29 18:11:17 +0300

    feat: add info about admins and sudoers

--- install-with-admin.zsh
+++ install-with-admin.zsh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+echo "**** Get list of sudoers ****"
+sudo cat /etc/sudoers
+# As usual, a user can be added to the list of sudoers by adding a line such as this (spaces are optional)
+# username ALL=(ALL) ALL
+# username ALL = (ALL) ALL
+
+echo "**** Get list of admins ****"
+dscl . -read /groups/admin | grep GroupMembership
+
 echo "**** Anki ****"
 brew install --cask anki
 
