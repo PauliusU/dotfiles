d574c4b1b0ba11153e95c5706bf4c2176bce9d58
Paulius Uosis <48020370+PauliusU@users.noreply.github.com>
2022-09-13 18:14:22 +0300

    feat: add neovim

--- settings.zsh
+++ settings.zsh
@@ -88,9 +88,10 @@ echo "Add symlinks for common folders"
 ln -s ~/Dropbox/code ~/code
 ln -s ~/Dropbox/dev ~/dev
 ln -s ~/Dropbox/soft/mpv_portable/portable_config/* ~/.config/mpv
-ln -s $(pwd)/marta ~/Library/Application\ Support/org.yanex.marta
-ln -s $(pwd)/espanso ~/Library/Application\ Support/espanso
 ln -s ~/dev/config/.aws ~/.aws
 ln -s $(pwd)/.hammerspoon ~/.hammerspoon
 ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 ln -s $(pwd)/.zsh_aliases ~/.zsh_aliases
+ln -s $(pwd)/espanso ~/Library/Application\ Support/espanso
+ln -s $(pwd)/marta ~/Library/Application\ Support/org.yanex.marta
+ln -s $(pwd)/nvim ~/.config/nvim
