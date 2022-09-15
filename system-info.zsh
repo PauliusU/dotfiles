3f85c719d57b9bb5376910a47d854d884501d7e4
Paulius Uosis <paulius.uosis@vialet.eu>
2022-09-15 09:42:09 +0300

    feat: add system-info script

---
+++ system-info.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+sw_vers # macOs and build version
+sw_vers -productVersion # macOs version only
+uname -a # Info about Darwin (Unix base for macOS)
+system_profiler SPSoftwareDataType # OS version, computer name, user name, etc.
