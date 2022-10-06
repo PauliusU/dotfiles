a3f9ecd8367d89a6358325f386d49b585785c151
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-06 11:21:15 +0300

    refactor: group system-info.zsh settings by OS

--- scripts/system-info.zsh
+++ scripts/system-info.zsh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 
-# Get basic info about the system
+# Get basic info about the system (e.g. OS version, build)
 
-sw_vers # macOs and build version
-sw_vers -productVersion # macOs version only
 uname -a # Info about Darwin (Unix base for macOS)
-system_profiler SPSoftwareDataType # OS version, computer name, user name, etc.
+if [[ "$(uname)" == "Darwin" ]]; then
+    echo "macOs"
+    sw_vers # macOs and build version
+    sw_vers -productVersion # macOs version only
+    system_profiler SPSoftwareDataType # OS version, computer name, user name, etc.
+fi
