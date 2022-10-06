36dbaa395dcbd3703ddd4930afccce8c2e28a1c5
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-06 11:31:23 +0300

    refactor: move scripts to a separate folder

--- setup.zsh
+++ setup.zsh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 
-source file-associations.zsh
+source $(dirname "$0")/scripts/file-system.zsh
+source $(dirname "$0")/scripts/install-soft.zsh
+source $(dirname "$0")/scripts/file-associations.zsh
+source $(dirname "$0")/scripts/settings-macos.zsh
