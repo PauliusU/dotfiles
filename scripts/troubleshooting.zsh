5b1fbbb8c6614d05e7d58c286d486b6364912101
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-06 11:19:14 +0300

    feat: clean apt-get packages

--- scripts/troubleshooting.zsh
+++ scripts/troubleshooting.zsh
@@ -1,8 +1,16 @@
 #!/bin/zsh
 
+# Toubleshoot system problems by reseting settings and cleaning up the software
+
 # Reset all accesability settings
 sudo tccutil reset Accessibility
 
 # Uninstall all apps from macports
 sudo port -fp uninstall installed
 port installed
+
+if [[ $(uname) == "Linux" ]]; then
+    sudo apt-get -y autoremove
+    sudo apt-get -y autoclean
+    sudo apt-get -y clean
+fi
