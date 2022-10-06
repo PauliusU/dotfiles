31a9e2a115690613eb66afcae01f0103ea65811e
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-06 11:24:23 +0300

    refactor: extract file-system settings

---
+++ scripts/file-system.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+echo "Add symlinks for common folders"
+ln -s ~/Dropbox/code ~/code
+ln -s ~/Dropbox/dev ~/dev
+ln -s ~/Dropbox/soft/mpv_portable/portable_config/* ~/.config/mpv
+ln -s ~/dev/config/.aws ~/.aws
+ln -s $(pwd)/.hammerspoon ~/.hammerspoon
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+ln -s $(pwd)/.zsh_aliases ~/.zsh_aliases
+ln -s $(pwd)/nvim ~/.config/nvim
+ln -s $(pwd)/nvim ~/.vim
+ln -s $(pwd)/nvim/init.vim ~/.vimrc
+if [[ "$(uname)" == "Darwin" ]]; then
+    ln -s $(pwd)/espanso ~/Library/Application\ Support/espanso
+    ln -s $(pwd)/marta ~/Library/Application\ Support/org.yanex.marta
+fi
