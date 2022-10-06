501c68a4148ca18d1bcbc8f5dcf4f4db65a0d032
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-06 11:18:40 +0300

    fix: paths inside fs.zsh

--- fs.zsh
+++ fs.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 # File system
-python3 ~/code/OS-config/fs_dropbox_ignore_node_modules.py
-python3 ~/code/OS-config/fs_long_path_names_checker.py
+python3 ~/code/OS-config/scripts/fs_dropbox_ignore_node_modules.py
+python3 ~/code/OS-config/scripts/fs_long_path_names_checker.py
 
 # Software updates
 brew update && brew upgrade && brew upgrade --cask && brew cleanup
