e7dd38258fb81e59f2ef3e57e1aba975378a1c7d
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-06 11:48:32 +0300

    feat: check uname an ostype in system-info script

--- scripts/system-info.zsh
+++ scripts/system-info.zsh
@@ -1,11 +1,31 @@
-#!/bin/zsh
+#!/bin/sh
 
-# Get basic info about the system (e.g. OS version, build)
+# os-check - get basic info about OS running the script, OS version, build.
 
-uname -a # Info about Darwin (Unix base for macOS)
+echo "uname: $(uname)" # Linux, Darwin, mysu
+echo "OSTYPE: $OSTYPE" # Linux, darwin21, msys
+uname -a
+
+case "$(uname -sr)" in # # -s OS name -r OS release. E.g. "Darwin 21.6.0"
+Darwin*)
+    echo 'macOs / Mac OS X'
+    ;;
+Linux*Microsoft*)
+    echo 'WSL1' # Windows Subsystem for Linux
+    ;;
+Linux*)
+    echo 'Linux'
+    ;;
+CYGWIN* | MINGW* | MINGW32* | MSYS*)
+    echo 'MS Windows'
+    ;;
+*)
+    echo 'Other OS'
+    ;;
+esac
+
 if [[ "$(uname)" == "Darwin" ]]; then
-    echo "macOs"
-    sw_vers # macOs and build version
-    sw_vers -productVersion # macOs version only
+    sw_vers                            # macOs and build version
+    sw_vers -productVersion            # macOs version only
     system_profiler SPSoftwareDataType # OS version, computer name, user name, etc.
 fi
