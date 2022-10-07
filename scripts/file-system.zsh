24cd148c331bdcceea068e83b91c852450d5fd66
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-07 08:56:30 +0300

    feat: backup file before making symlinks

--- scripts/file-system.zsh
+++ scripts/file-system.zsh
@@ -5,12 +5,19 @@ ln -s ~/Dropbox/code ~/code
 ln -s ~/Dropbox/dev ~/dev
 ln -s ~/Dropbox/soft/mpv_portable/portable_config/* ~/.config/mpv
 ln -s ~/dev/config/.aws ~/.aws
+mv ~/.hammerspoon ~/.hammerspoon.bak
 ln -s $(pwd)/.hammerspoon ~/.hammerspoon
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/.zsh_aliases ~/.bash_aliases # Zsh aliases in Bash
-ln -s $(pwd)/.zsh_aliases ~/.zsh_aliases
+mv ~/.tmux.conf ~/.tmux.conf.bak
+ln -s $(pwd)/terminal/.tmux.conf ~/.tmux.conf
+mv ~/.shellrc ~/.shellrc.bak
+ln -s $(pwd)/terminal/.shellrc ~/.shellrc
+mv ~/.aliases ~/.aliases.bak
+ln -s $(pwd)/terminal/.aliases ~/.aliases
+mv ~/.config/nvim ~/.config/nvim.bak
 ln -s $(pwd)/nvim ~/.config/nvim
+mv ~/.vim ~/.vim.bak
 ln -s $(pwd)/nvim ~/.vim
+mv ~/.vimrc ~/.vimrc.bak
 ln -s $(pwd)/nvim/init.vim ~/.vimrc
 if [[ "$(uname)" == "Darwin" ]]; then
     ln -s $(pwd)/espanso ~/Library/Application\ Support/espanso
