575f29929928e091f52ccc1620aa7e7eedd20ef7
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-07 08:52:51 +0300

    refactor: rename fs script

--- scripts/fs.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# File system
-python3 ~/code/OS-config/scripts/fs_dropbox_ignore_node_modules.py
-python3 ~/code/OS-config/scripts/fs_long_path_names_checker.py
-
-# Software updates
-brew update && brew upgrade && brew upgrade --cask && brew cleanup
-softwareupdate -ai
