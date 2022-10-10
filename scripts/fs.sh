b24d49ed31de4c9d772968a00c5a05d50b08a6b9
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-10 09:52:05 +0300

    fix(shell): fs.sh alias

--- scripts/fs.sh
+++ scripts/fs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # File system
-python3 ~/code/dotfiles/scripts/fs_dropbox_ignore_node_modules.py
-python3 ~/code/doftiles/scripts/fs_long_path_names_checker.py
+python3 ~/code/dotfiles/scripts/fs_dropbox_ignore_node_modules.private.py
+python3 ~/code/dotfiles/scripts/fs_long_path_names_checker.private.py
 
 # Software updates
 brew update && brew upgrade && brew upgrade --cask && brew cleanup
