7dc9f6ca448a162da1a97142da55b96e8c34f31c
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-15 23:41:43 +0300

    fix: dev folder symlink

--- setup.zsh
+++ setup.zsh
@@ -2,7 +2,7 @@
 
 # Symlinks for common files and folders. -f forces linking if file aready exists.
 ln -sf ~/Dropbox/code ~/code
-ln -sf ~/Dropbox/dev ~/devs
+ln -sf ~/Dropbox/dev ~/dev
 
 source $(dirname "$0")/scripts/file-system.zsh
 source $(dirname "$0")/scripts/install-soft.zsh
