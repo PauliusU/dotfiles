0657252b89284a96e426d1e515a1153ded4def0c
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-10-31 11:08:26 +0200

    refactor: macOS settings script

--- scripts/settings-macos.zsh
+++ scripts/settings-macos.zsh
@@ -1,24 +1,7 @@
 #!/bin/zsh
+# macOS system settings
 
-# macOS specific system preferences
-
-echo "**** Scroll direction - Disable “natural” (Lion-style) scrolling ****"
-# defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-defaults write -g com.apple.swipescrolldirection -bool FALSE
-defaults read -g com.apple.swipescrolldirection
-
-echo "**** Enable LT-US keyboard ****"
-defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleCurrentKeyboardLayoutInputSourceID "org.unknown.keylayout.LithuanianUSkeyboardwithLithuanianletters"
-defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleDefaultAsciiInputSource -dict InputSourceKind "Keyboard Layout" "KeyboardLayout ID" -int 16383 "KeyboardLayout Name" "Lithuanian US keyboard with Lithuanian letters"
-
-defaults delete "~/Library/Preferences/com.apple.HIToolbox" AppleEnabledInputSources
-
-defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleEnabledInputSources -array "{ InputSourceKind = \"Keyboard Layout\"; \"KeyboardLayout ID\" = 16383; \"KeyboardLayout Name\" = \"Lithuanian US keyboard with Lithuanian letters\"; }"
-defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleInputSourceHistory -array "{ InputSourceKind = \"Keyboard Layout\"; \"KeyboardLayout ID\" = 16383; \"KeyboardLayout Name\" = \"Lithuanian US keyboard with Lithuanian letters\"; }"
-defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleSelectedInputSources -array "{ InputSourceKind = \"Keyboard Layout\"; \"KeyboardLayout ID\" = 16383; \"KeyboardLayout Name\" = \"Lithuanian US keyboard with Lithuanian letters\"; }"
-
-defaults read ~/Library/Preferences/com.apple.HIToolbox
-
+# General => Language & Region
 echo "**** Set language and text formats ****"
 # Settings => Language & Region
 # defaults write NSGlobalDomain AppleLanguages -array "en-LT" "lt-LT" "ru-LT"
@@ -32,28 +15,30 @@ defaults read NSGlobalDomain AppleMeasurementUnits
 defaults write NSGlobalDomain AppleMetricUnits -bool true
 defaults read NSGlobalDomain AppleMetricUnits
 
-echo "======================= FINDER AND DESKTOP =============================="
+# Accessibility => Display => Reduce motion
+echo "**** Reduce aninmations ****"
+defaults write com.apple.universalaccess reduceMotion -bool true
+defaults read com.apple.universalaccess reduceMotion
 
-# echo "**** Allow exiting Fnder  via ⌘ + Q; doing so will also hide desktop icons ****"
-# defaults write com.apple.finder QuitMenuItem -bool NO
-# defaults write com.apple.finder QuitMenuItem -bool true
-defaults read com.apple.finder QuitMenuItem
+# Desktop & Dock => Dock
+echo "**** Enable Dock autohide ****"
+defaults write com.apple.dock autohide -bool true
+defaults read com.apple.dock autohide
 
+# Desktop & Dock => Menu Bar
 echo "**** Auto hide menu bar ****"
 defaults write NSGlobalDomain _HIHideMenuBar -bool true
 defaults read NSGlobalDomain _HIHideMenuBar
 killall Finder
 
-echo "**** Enable Dock autohide ****"
-defaults write com.apple.dock autohide -bool true
-defaults read com.apple.dock autohide
-
+# Desktop & Dock => Mission control
 echo "**** Group windows by application in Mission Control ****"
 defaults write com.apple.dock "expose-group-by-app" -bool true
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
+# Desktop & Dock => Hot corners
 echo "**** Hot corners ****"
 # => Settings => Mission Control => Hot corners
 # Possible values:
@@ -78,7 +63,16 @@ defaults write com.apple.dock wvous-bl-corner -int 11
 defaults write com.apple.dock wvous-bl-corner -int 4
 defaults read com.apple.dock | grep wvous
 
-echo "**** Animations ****"
+# Desktop & Dock => options inaccesible in the UI
+echo "**** Allow exiting Finder via ⌘ + Q; doing so will also hide desktop icons ****"
+# defaults write com.apple.finder QuitMenuItem -bool true
+defaults read com.apple.finder QuitMenuItem
+# defaults write com.apple.finder QuitMenuItem -bool NO # Default value
+
+echo "**** Show the ~/Library folder ****"
+chflags nohidden ~/Library
+
+echo "**** Reduce animations ****"
 # Finder: disable window animations and Get Info animations
 defaults write com.apple.finder DisableAllAnimations -bool true
 defaults read com.apple.finder | grep DisableAllAnimations
@@ -87,12 +81,27 @@ defaults read com.apple.finder | grep DisableAllAnimations
 defaults write com.apple.dock expose-animation-duration -float 0.1
 defaults read com.apple.dock expose-animation-duration
 
-# Accessibility => Display => Reduce motion
-defaults write com.apple.universalaccess reduceMotion -bool true
-defaults read com.apple.universalaccess reduceMotion
-
-# Hide single external drive from desktop
+echo "***** Hide single external drive from desktop ****"
 sudo chflags hidden /Volumes/SSD4
 killall Finder
 # # Unhide
 # sudo chflags nohidden /Volumes/SSD4
+
+# Keyboard => Text input
+echo "**** Enable LT-US keyboard ****"
+defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleCurrentKeyboardLayoutInputSourceID "org.unknown.keylayout.LithuanianUSkeyboardwithLithuanianletters"
+defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleDefaultAsciiInputSource -dict InputSourceKind "Keyboard Layout" "KeyboardLayout ID" -int 16383 "KeyboardLayout Name" "Lithuanian US keyboard with Lithuanian letters"
+
+defaults delete "~/Library/Preferences/com.apple.HIToolbox" AppleEnabledInputSources
+
+defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleEnabledInputSources -array "{ InputSourceKind = \"Keyboard Layout\"; \"KeyboardLayout ID\" = 16383; \"KeyboardLayout Name\" = \"Lithuanian US keyboard with Lithuanian letters\"; }"
+defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleInputSourceHistory -array "{ InputSourceKind = \"Keyboard Layout\"; \"KeyboardLayout ID\" = 16383; \"KeyboardLayout Name\" = \"Lithuanian US keyboard with Lithuanian letters\"; }"
+defaults write "~/Library/Preferences/com.apple.HIToolbox" AppleSelectedInputSources -array "{ InputSourceKind = \"Keyboard Layout\"; \"KeyboardLayout ID\" = 16383; \"KeyboardLayout Name\" = \"Lithuanian US keyboard with Lithuanian letters\"; }"
+
+defaults read ~/Library/Preferences/com.apple.HIToolbox
+
+# Trackpad => Scroll & Zoom => Natural scolling
+echo "**** Scroll direction - Disable “natural” (Lion-style) scrolling ****"
+# defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+defaults write -g com.apple.swipescrolldirection -bool FALSE
+defaults read -g com.apple.swipescrolldirection
