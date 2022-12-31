55bdb0deac8c3dd392ed5ea6974ed9628f8b3bbc
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-12-31 17:56:20 +0200

    feat(nvim): shortcut to enter normal mode

--- nvim/lua/nvimconfig/remap.lua
+++ nvim/lua/nvimconfig/remap.lua
@@ -1,7 +1,8 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<leader>r", "<cmd>:source $MYVIMRC<CR>") -- Reload config
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Explore file tree (project view)
+vim.keymap.set("i", "jk", "<Esc>") -- Enter normal mode
 
 -- Move lines in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -40,7 +41,7 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Rename symbol (replace word under the cursor)
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>n", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- Make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
