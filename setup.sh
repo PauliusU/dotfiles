90834ec8726b75f31608d614edc3ac6042c37bb0
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-01-18 12:05:42 +0200

    feat(shell): add WSL specific symlinks

--- setup.sh
+++ setup.sh
@@ -4,6 +4,13 @@
 ln -sf ~/Dropbox/code ~/code
 ln -sf ~/Dropbox/dev ~/dev
 
+if [[ "$(uname -sr)" == "Linux"*"microsoft"* ]]; then
+    echo "WSL found"
+    ln -sf /mnt/d/Dropbox ~/Dropbox
+    ln -sf /mnt/d/Dropbox/code ~/code
+    ln -sf /mnt/d/Dropbox/dev ~/dev
+fi
+
 source $(dirname "$0")/scripts/file-system.zsh
 source $(dirname "$0")/scripts/install-soft.zsh
 source $(dirname "$0")/scripts/file-associations.zsh
