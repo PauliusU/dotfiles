a12f630628943fca4359d6b837fa39ebdbbb72e4
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-02-17 09:42:28 +0200

    feat(nvim): add code-runner plugin

---
+++ nvim/after/plugin/code-runner.lua
@@ -0,0 +1,17 @@
+require('code_runner').setup({
+    -- put here the commands by filetype
+    filetype = {
+        java = "cd $dir && javac $fileName && java $fileNameWithoutExt",
+        python = "python3 -u",
+        typescript = "deno run",
+        rust = "cd $dir && rustc $fileName && $dir/$fileNameWithoutExt"
+    },
+})
+
+vim.keymap.set('n', '<leader>rr', ':RunCode<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
