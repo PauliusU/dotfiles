f5d7dfa7fd191f132a599685d406db42b417e487
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-02-24 11:05:20 +0200

    feat(nvim): numToStr/Comment.nvim plugin

---
+++ nvim/after/plugin/comment.lua
@@ -0,0 +1,7 @@
+require('Comment').setup()
+
+-- TODO add custom keymaps 
+-- Comment and stay in the same line with Ctrl + / (despite _ in the shortcut)
+-- vim.keymap.set({'n', 'i', 'v'}, '<C-_>', '')
+-- Comment and move to the next line with Alt - /
+-- vim.keymap.set({'n', 'i', 'v'}, '<A-/>', '')
