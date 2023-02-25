3fc38d532697d69d792c29694ea179e5d0654098
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-02-25 19:58:15 +0200

    feat: scoop and ahk config for windows

---
+++ setup-windows/base-4-ahk.setup.bat
@@ -0,0 +1,13 @@
+@ECHO OFF &:: Clean output and hide path of running command
+
+ECHO **** AHK ****
+
+powershell scoop install autohotkey
+
+:: Run AHK
+START "%SCOOP%\apps\autohotkey\current\v2\AutoHotkey64.exe" "%DOTFILES%\ahk\main.ahk"
+
+:: AHK system startup link
+SET TARGET='%DOTFILES%\ahk\main.ahk'
+SET SHORTCUT='%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\AHK_MAIN_startup.lnk'
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%SHORTCUT%); $S.TargetPath = %TARGET%; $S.Save()"
