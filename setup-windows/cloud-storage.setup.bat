a6e95a2fcd27399db74324ad53acfc535828960a
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-03-02 23:53:55 +0200

    feat: browsers and cloud storage setup

---
+++ setup-windows/cloud-storage.setup.bat
@@ -0,0 +1,20 @@
+@ECHO OFF &:: Clean output and hide path of running command
+
+ECHO **** Dropbox ****
+@REM REG IMPORT "d:\Dropbox\code\dotfiles\Dropbox_HOME_backup.reg"
+@REM REG IMPORT "d:\Dropbox\code\dotfiles\DropboxUpdate_HOME_backup.reg"
+MKLINK /J "%APPDATA%\Dropbox" "%USER_BAK%\AppData\Roaming\Dropbox"
+MKLINK /J "%LOCALAPPDATA%\Dropbox" "%USER_BAK%\AppData\Local\Dropbox"
+MKLINK /J "%USERPROFILE%\code" "d:\Dropbox\code"
+MKLINK /J "%USERPROFILE%\dev" "d:\Dropbox\dev"
+MKLINK /J "%USERPROFILE%\Dropbox" "d:\Dropbox"
+MKLINK /J "%USERPROFILE%\media" "d:\Dropbox\DropsyncFiles\media"
+winget install Dropbox.Dropbox
+
+ECHO **** Google Drive ****
+MKLINK /J "%USERPROFILE%\Google Drive" "D:\linked\gDrive"
+MKLINK /J "%USERPROFILE%\gDrive" "D:\linked\gDrive"
+IF NOT EXIST "%LOCALAPPDATA%\Google" MKDIR "%LOCALAPPDATA%\Google"
+:: WARN Google Drive does not work with folder junctions
+XCOPY /E /I "%USER_BAK%\AppData\Local\Google\DriveFS" "%LOCALAPPDATA%\Google\DriveFS"
+winget install Google.Drive
