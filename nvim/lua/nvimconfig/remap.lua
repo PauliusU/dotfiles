6d62a9525d0f750cabc017193bccdf7ecb8e6f02
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-03-13 09:29:54 +0200

    refactor(nvim): keyboard shortcuts

--- nvim/lua/nvimconfig/remap.lua
+++ nvim/lua/nvimconfig/remap.lua
@@ -1,63 +1,75 @@
+-- Custom keyboard shortcuts for neovim
+
 -- Set <space> as the leader key
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>r", "<cmd>source $MYVIMRC<CR>") -- Reload config
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Explore file tree (project view)
-vim.keymap.set("i", "jk", "<Esc>") -- Enter normal mode
+-- Manage config
+vim.keymap.set("n", "<leader>r", "<cmd>source $MYVIMRC<CR>", { desc = 'Reload config' })
+vim.keymap.set('n', '<leader>l', '<cmd>source %<CR>', { desc = '[L]oad file a.k.a source active buffer' })
+
+-- Switch modes
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = 'Explore file tree ([P]roject [V]view)' })
+vim.keymap.set("i", "jk", "<Esc>", { desc = 'Exist insert mode' })
+vim.keymap.set("t", "jk", "<C-\\><C-n>", { desc = 'Exit terminal mode' })
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = 'Enter normal mode' })
 
 -- Navigation
+vim.keymap.set({ "n", "v", "i" }, "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>", { desc = "Switch tmux sessions" })
+-- Switch buffers
+local default_opts = { noremap = true, silent = true }
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", default_opts)
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", default_opts)
 vim.keymap.set({ "n", "v", "i" }, "[b", "<cmd>bp<CR>", { desc = "Previous buffer :bprevious" })
 vim.keymap.set({ "n", "v", "i" }, "]b", "<cmd>bn<CR>", { desc = "Next buffer :bnext" })
+vim.keymap.set({ "n" }, "6", "<cmd>b#<CR>", { desc = "Toggle between two buffers. <C-^> in other layout." })
+-- Switch tabs
+vim.keymap.set({ "n" }, "]t", "<cmd>tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set({ "n" }, "[t", "<cmd>tabprevious<CR>", { desc = "Previous tab" })
+-- Quickfix navigation
+vim.keymap.set("n", "<C-A-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-A-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- Move lines in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
+-- Move selected line / block of text in visual mode
+vim.keymap.set("x", "K", ":move '<-2<CR>gv-gv", default_opts)
+vim.keymap.set("x", "J", ":move '>+1<CR>gv-gv", default_opts)
 
-vim.keymap.set("n", "J", "mzJ`z") -- Append line below, but keep cursor in space
+-- Resizing panes
+vim.keymap.set("n", "<Left>", ":vertical resize +1<CR>", default_opts)
+vim.keymap.set("n", "<Right>", ":vertical resize -1<CR>", default_opts)
+vim.keymap.set("n", "<Up>", ":resize -1<CR>", default_opts)
+vim.keymap.set("n", "<Down>", ":resize +1<CR>", default_opts)
 
 -- Stay in the middle when navigating or searching
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "n", "nzzzv", { desc = 'Center search results' })
+vim.keymap.set("n", "N", "Nzzzv", { desc = 'Center search results' })
 
--- greatest remap ever -- paste without messing your register
-vim.keymap.set("x", "<leader>p", [["_dP]])
+-- Copy, paste, delete
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = 'Delete to void register' })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste without messing your register' })
+vim.keymap.set("v", "p", '"_dP', { desc = "Paste over currently selected text without yanking it" })
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = 'asbjornHaland -- copy to system clipboard #1' })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = 'asbjornHaland -- copy to system clipboard #2' })
 
--- next greatest remap ever : asbjornHaland -- copy to system clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
--- Delete to void register
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-
--- This is going to get me cancelled
--- vim.keymap.set("i", "<C-c>", "<Esc>")
-
-vim.keymap.set("n", "Q", "<nop>") -- disable capital Q
-vim.keymap.set({ "n", "v", "i" }, "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>o", vim.lsp.buf.format) -- Format current buffer
-
--- Quickfix navigation
--- TODO resolve conflict with harpoon shortcuts
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
--- Rename symbol (replace word under the cursor)
-vim.keymap.set("n", "<leader>n", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- Make file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
--- -- Similar to other editors
--- Save with Alt-S
+-- Similar to other editors (save, quit, select all)
 vim.keymap.set({ "n", "v", "i" }, "<A-s>", vim.cmd.update)
--- Quit with Alt-Q
-vim.keymap.set({ "n", "v", "i" }, "<A-q>", "<cmd>q<CR>")
--- Force quit with Ctrl-Alt-Q
-vim.keymap.set({ "n", "v", "i" }, "<C-A-q>", "<cmd>q!<CR>")
--- Select all with Alt-A
-vim.keymap.set({ "n", "v" }, "<A-a>", "<esc>ggVG")
+vim.keymap.set({ "n", "v", "i" }, "<A-q>", "<cmd>q<CR>", { desc = '[Q]uit' })
+vim.keymap.set({ "n", "v", "i" }, "<C-A-q>", "<cmd>q!<CR>", { desc = 'Force [q]uit' })
+vim.keymap.set({ "n", "v" }, "<A-a>", "<esc>ggVG", { desc = 'Select [A]ll text' })
 
-vim.keymap.set({ "n", "v" }, "<A-m>", "<cmd>set wrap!<CR>") -- Toggle wrap
+-- Etc
+vim.keymap.set("n", "Q", "<nop>", { desc = 'Disable capital Q' })
+vim.keymap.set("n", "<leader>o", vim.lsp.buf.format, { desc = 'F[o]rmat current buffer' })
+vim.keymap.set({ "n", "v" }, "<A-m>", "<cmd>set wrap!<CR>", { desc = 'Toggle wrap' })
+vim.keymap.set("n", "<leader>n", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+{ desc = "Rename symbol (replace word under the cursor)" })
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make file executable" })
+vim.keymap.set("n", "J", "mzJ`z", { desc = 'Append line below, but keep cursor in place' })
+vim.keymap.set("v", "<", "<gv", default_opts)
+vim.keymap.set("v", ">", ">gv", default_opts)
