8887358f7e8297739446018b9596b7afbf5f3083
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-03-14 17:34:38 +0200

    feat(nvim): gutter icons for diagnostics messages

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -9,7 +9,7 @@ lsp.ensure_installed({
     'rust_analyzer',
 })
 
--- Fix Undefined global 'vim' and hammerspoon 'hs'
+-- Fix undefined global 'vim' and hammerspoon 'hs'
 lsp.configure('lua_ls', {
     settings = {
         Lua = {
@@ -20,55 +20,47 @@ lsp.configure('lua_ls', {
     }
 })
 
-
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-i>'] = cmp.mapping.complete(),
 })
-
--- -- disable completion with tab
--- -- this helps with copilot setup
--- cmp_mappings['<Tab>'] = nil
--- cmp_mappings['<S-Tab>'] = nil
 
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings
 })
 
--- lsp.set_preferences({
---     suggest_lsp_servers = false,
---     sign_icons = {
---         error = 'E',
---         warn = 'W',
---         hint = 'H',
---         info = 'I'
---     }
--- })
+lsp.set_preferences({
+    suggest_lsp_servers = true,
+    sign_icons = {
+        error = '✘',
+        warn = '▲',
+        hint = '⚑',
+        info = ''
+    }
+})
 
 lsp.on_attach(function(client, bufnr)
     print('LSP loaded: ' .. client.name)
 
-    local opts = { buffer = bufnr, remap = false }
+    -- if client.name == "eslint" then
+    --     vim.cmd.LspStop('eslint')
+    --     return
+    -- end
 
-    if client.name == "eslint" then
-        vim.cmd.LspStop('eslint')
-        return
-    end
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = 'LSP [G]o to [D]efinition', buffer = bufnr, remap = false })
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = 'LSP hover', buffer = bufnr, remap = false })
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, { desc = 'LSP workspace symbol', buffer = bufnr, remap = false })
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = 'Diagnotsic: open float', buffer = bufnr, remap = false })
     vim.keymap.set("n", "[d", vim.diagnostic.goto_next, { desc = 'Next diagnostic', buffer = bufnr, remap = false })
     vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, { desc = 'Prev diagnostic', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, { desc = 'LSP code action', buffer = bufnr, remap = false })
+    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, { desc = 'LSP references', buffer = bufnr, remap = false })
+    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, { desc = 'LSP rename', buffer = bufnr, remap = false })
+    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, { desc = 'LSP signature help', buffer = bufnr, remap = false })
 end)
 
 lsp.setup()
