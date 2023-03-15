e5abcd6c79fef9c9a046abd0857044a649b0d105
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-03-15 09:34:47 +0200

    feat(nvim): todo-comments config

--- nvim/after/plugin/todo-comments.lua
+++ nvim/after/plugin/todo-comments.lua
@@ -1,3 +1,22 @@
-require('todo-comments').setup()
+require('todo-comments').setup({
+    highlight = {
+        pattern = [[.*<(KEYWORDS)\s*]], -- pattern or table of patterns, used for highlighting (vim regex)
+    },
+    search = {
+        pattern = [[\b(KEYWORDS)\b]], -- match without the extra colon. You'll likely get false positives
+    }
+})
 
-vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope<CR>', { desc = '[S]earch [T]odos'})
+local jumpKeywords = { keywords = { "FIX", "TEST", "TODO", "WARNING" } }
+local function prevTodo()
+    require("todo-comments").jump_prev(jumpKeywords)
+end
+local function nextTodo()
+    require("todo-comments").jump_next(jumpKeywords)
+end
+
+vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope<CR>', { desc = '[S]earch [T]odos' })
+vim.keymap.set('n', '<leader>tt', '<cmd>TodoQuickFix<CR>', { desc = 'Add TODOs to quickfix list' })
+vim.keymap.set('n', '<leader>tl', '<cmd>TodoLocList<CR>', { desc = 'Show location list to show project TODOs' })
+vim.keymap.set("n", "]t", nextTodo, { desc = "Next todo comment" })
+vim.keymap.set("n", "[t", prevTodo, { desc = "Previous todo comment" })
