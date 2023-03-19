9ba755bff500747e871c5f2e4da76ccba5c8c221
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-03-19 22:52:47 +0200

    feat: file management setup for Windows

---
+++ setup-windows/file-management.setup.bat
@@ -0,0 +1,51 @@
+@ECHO OFF &:: Clean output and hide path of running command
+
+TITLE FILE MANAGEMENT AND SEARCH
+
+ECHO **** 7z / 7zip / Archive.****
+powershell scoop install 7zip
+ASSOC .7z=Archive.7z
+FTYPE Archive.7z="%SCOOP%\apps\7zip\current\7zFM.exe" "%%1"
+ASSOC .zip=7-Archive.ZIP
+FTYPE Archive.ZIP="%SCOOP%\apps\7zip\current\7zFM.exe" "%%1"
+ASSOC .rar=7-Archive.RAR
+FTYPE Archive.RAR="%SCOOP%\apps\7zip\current\7zFM.exe" "%%1"
+:: .zip
+REG ADD "HKCR\Archive.ZIP\shell\E&xtract to folder (7Z)\command" /VE /t REG_SZ /D "%SCOOP%\apps\7zip\current\7z.exe x \"%%1\" -o* -aou" /F
+REG ADD "HKCR\Archive.ZIP\shell\E&xtract to folder (7Z)" /V Icon /D "%SCOOP%\apps\7zip\current\7zip.ico" /F
+:: .7z
+REG ADD "HKCR\Archive.7z\shell\E&xtract to folder (7Z)\command" /VE /T REG_SZ /d "%SCOOP%\apps\7zip\current\7z.exe x \"%%1\" -o* -aou" /F
+REG ADD "HKCR\Archive.7z\shell\E&xtract to folder (7Z)" /V Icon /D "%SCOOP%\apps\7zip\current\7zip.ico" /F
+:: .rar
+REG ADD "HKCR\Archive.rar\shell\E&xtract to folder (7Z)\command" /VE /T REG_SZ /d "%SCOOP%\apps\7zip\current\7z.exe x \"%%1\" -o* -aou" /F
+REG ADD "HKCR\Archive.rar\shell\E&xtract to folder (7Z)" /V Icon /D "%SCOOP%\apps\7zip\current\7zip.ico" /F
+:: .exe with 7z
+REG ADD "HKCR\exefile\shell\E&xtract to folder (7Z)\command" /VE /T REG_SZ /D "%SCOOP%\apps\7zip\current\7z.exe x \"%%1\" -o* -aou" /F
+REG ADD "HKCR\exefile\shell\E&xtract to folder (7Z)" /V Icon /D "%SCOOP%\apps\7zip\current\7zip.ico" /F
+
+ECHO **** Everything file search ****
+powershell scoop bucket add versions
+powershell scoop install everything-alpha
+REG IMPORT "%SCOOP%\apps\everything-alpha\current\install-context.reg"
+:: powershell scoop install everything
+
+ECHO *** Launchy ****
+powershell scoop install launchy
+SET TARGET='%SCOOP%\apps\launchy\current\Launchy.exe'
+SET SHORTCUT='%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Launchy_startup.lnk'
+powershell -NoProfile -ExecutionPolicy Bypass -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut(%SHORTCUT%); $S.TargetPath = %TARGET%; $S.Save()"
+
+ECHO **** SpaceSniffer ****
+powershell scoop install spacesniffer
+REG ADD "HKEY_CLASSES_ROOT\Folder\shell\SpaceSniffer" /VE /D "&SpaceSniffer Here" /F
+REG ADD "HKEY_CLASSES_ROOT\Folder\shell\SpaceSniffer" /V Icon /D "%SCOOP%\apps\spacesniffer\current\SpaceSniffer.exe" /F
+REG ADD "HKEY_CLASSES_ROOT\Folder\shell\SpaceSniffer\command" /ve /t REG_SZ /d "%SCOOP%\apps\spacesniffer\current\SpaceSniffer.exe scan \"%%1\"" /f
+
+ECHO **** Midnight commander ****
+powershell scoop install mc
+
+ECHO **** Unlocker in context menu ****
+powershell scoop install unlocker
+REG ADD "HKEY_CLASSES_ROOT\Directory\shell\Unlocker" /VE /D "Send to &Unlocker" /F
+REG ADD "HKEY_CLASSES_ROOT\Directory\shell\Unlocker" /v Icon /d "%SCOOP%\apps\unlocker\current\Unlocker.exe" /F
+REG ADD "HKEY_CLASSES_ROOT\Directory\shell\Unlocker\Command" /ve /d "%SCOOP%\apps\unlocker\current\Unlocker.exe \"%%1\"" /F
