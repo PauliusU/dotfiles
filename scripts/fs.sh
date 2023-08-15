0eb2c1b6b3698043f91d5e193d6d541e6b096bc9
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-08-15 16:53:12 +0300

    feat: add backup script

--- scripts/fs.sh
+++ scripts/fs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "**** File system ****"
-$DOTFILES/scripts/private/dropboxIgnore.js
-$DOTFILES/scripts/fs_dropbox_ignore_node_modules.private.py
-$DOTFILES/scripts/fs_long_path_names_checker.private.py
+node $DOTFILES/scripts/private/dropboxIgnore.js
+python $DOTFILES/scripts/fs_dropbox_ignore_node_modules.private.py
+python $DOTFILES/scripts/fs_long_path_names_checker.private.py
 
 echo "**** Backup ****"
-rsync -av --delete $HOME/Library/Preferences/doublecmd/ $HOME/Dropbox/linked/backup/doublecmd
+$DOTFILES/scripts/fs-backup.sh
 
 echo "**** Software updates ****"
-npm install -g npm-check-updates
 $DOTFILES/scripts/updater.sh
