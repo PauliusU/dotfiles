b52f257b65981216df17b086c97b1c9789564d16
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-08-20 00:23:20 +0300

    feat(nvim): filetype specific rules

---
+++ nvim/after/ftplugin/javascript.lua
@@ -0,0 +1,6 @@
+print('javascript.lua')
+
+vim.bo.tabstop = 2 -- size of a hard tabstop (ts).
+vim.bo.shiftwidth = 2 -- size of an indentation (sw).
+vim.bo.expandtab = true -- always uses spaces instead of tab characters (et).
+vim.bo.softtabstop = 2 -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
