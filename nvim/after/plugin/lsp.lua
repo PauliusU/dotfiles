268bea68f60649407ebf6af1c41c32f9ec94a1fb
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-08-22 17:54:19 +0300

    feat(nvim): review lsp related shortcuts

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,6 +1,6 @@
-local lsp = require("lsp-zero")
+local lsp = require('lsp-zero')
 
-lsp.preset("recommended")
+lsp.preset('recommended')
 
 lsp.ensure_installed({
     'tsserver',
@@ -23,6 +23,7 @@ lsp.configure('lua_ls', {
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-u>'] = cmp.mapping.abort(), -- Close suggestions pop-up
     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
@@ -44,30 +45,49 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-    print('LSP loaded: ' .. client.name)
+    -- client.name - lsp:server name. E.g 'eslint'
+    -- print('LSP loaded: ' .. client.name)
 
-    -- if client.name == "eslint" then
-    --     vim.cmd.LspStop('eslint')
-    --     return
-    -- end
+    -- LSP
+    -- LSP: definitions, declarations, references and symbols
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition,
+        { desc = 'lsp: [G]o to [D]efinition', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>gt', vim.lsp.buf.type_definition,
+        { desc = 'lsp: go to type [D]efinition', buffer = bufnr, remap = false })
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration,
+        { desc = 'lsp: [G]o to [D]eclaration', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>lgi', vim.lsp.buf.implementation,
+        { desc = 'lsp: [G]o to [I]mplementation', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references,
+        { desc = 'lsp: [G]o to [R]eferences', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>ws', vim.lsp.buf.workspace_symbol,
+        { desc = 'lsp: query [W]orkspace [S]ymbol', buffer = bufnr, remap = false })
+    -- LSP: actions and rename
+    vim.keymap.set('n', '<S-F6>', vim.lsp.buf.rename, { desc = 'lsp: rename', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<F1>', vim.lsp.buf.code_action,
+        { desc = 'lsp: code action', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<leader>h', vim.lsp.buf.code_action,
+        { desc = 'lsp: code action', buffer = bufnr, remap = false })
+    -- LSP: popups (hover, signature help)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { desc = 'lsp: hover', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<C-m>', vim.lsp.buf.signature_help,
+        { desc = 'lsp: signature help', buffer = bufnr, remap = false })
+    -- LSP: worspace folders
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, { desc = 'lsp: [W]orkspace [A]dd' })
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, { desc = 'lsp: [W]orkspace [R]emove' })
+    vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
+        { desc = 'lsp: [W]orkspace [L]ist' })
 
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = 'LSP [G]o to [D]eclaration', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = 'LSP [G]o to [D]efinition', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = 'LSP hover', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "<leader>lgi", vim.lsp.buf.implementation, { desc = 'LSP [G]o to [I]mplementation', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "<C-m>", vim.lsp.buf.signature_help, { desc = 'LSP signature help', buffer = bufnr, remap = false })
-    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, {desc = 'LSP [W]orkspace [A]dd' })
-    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, {desc = 'LSP [W]orkspace [R]emove' })
-    vim.keymap.set('n', '<leader>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, {desc = 'LSP [W]orkspace [L]ist' })
-    vim.keymap.set("n", "<leader>ws", vim.lsp.buf.workspace_symbol, { desc = 'LSP [W]orkspace [S]ymbol', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, { desc = '[L]SP type [D]efinition', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "<leader>ln", vim.lsp.buf.rename, { desc = '[L]SP [R]ename', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = 'LSP code action', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = 'LSP [G]o to [R]eferences', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, { desc = 'Diagnostic: open float', buffer = bufnr, remap = false })
+    -- Diagnostics (errors or warnings)
+    vim.keymap.set('n', '<leader>df', vim.diagnostic.open_float,
+        { desc = 'Diagnostic: open float', buffer = bufnr, remap = false })
     vim.keymap.set('n', '<leader>dl', vim.diagnostic.setloclist, { desc = 'Diagnostic: setloclist' })
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = 'Next diagnostic', buffer = bufnr, remap = false })
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = 'Prev diagnostic', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '<F2>', vim.diagnostic.goto_next,
+        { desc = 'Next diagnostic (problem)', buffer = bufnr, remap = false })
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next,
+        { desc = 'Next diagnostic (problem)', buffer = bufnr, remap = false })
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,
+        { desc = 'Prev diagnostic (problem)', buffer = bufnr, remap = false })
 end)
 
 lsp.setup()
