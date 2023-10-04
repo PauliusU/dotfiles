d1bb890b862c0df3314274687a4e70d1dacd2161
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-10-04 15:39:18 +0300

    feat(nvim): use lazygit instead of vim-fugitive

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -15,9 +15,34 @@ return {
     },
 
     -- Git and edit history
-    'tpope/vim-fugitive',
-    'lewis6991/gitsigns.nvim', -- Show which lines are currently changed
-    "sindrets/diffview.nvim",  -- View git diffs
+    {
+        "kdheepak/lazygit.nvim",
+        -- optional for floating window border decoration
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        -- Show which lines are currently changed
+        -- Adds git related signs to the gutter, as well as utilities for managing changes
+        'lewis6991/gitsigns.nvim',
+        opts = {
+            -- See `:help gitsigns.txt`
+            -- signs = {
+            --     add = { text = '+' },
+            --     change = { text = '~' },
+            --     delete = { text = '_' },
+            --     topdelete = { text = '‾' },
+            --     changedelete = { text = '~' },
+            -- },
+            on_attach = function(bufnr)
+                vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
+                    { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+                vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
+                    { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+                vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk,
+                    { buffer = bufnr, desc = '[P]review [H]unk' })
+            end,
+        },
+    },
     'mbbill/undotree',
 
     -- Comments, "gc" to comment visual regions/lines
