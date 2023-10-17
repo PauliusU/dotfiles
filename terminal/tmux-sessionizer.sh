a31414f8a716a424713bbe0e4449fc1cdcb67b67
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-10-17 17:53:12 +0300

    refactor: tmux-sessionizer variables

--- terminal/tmux-sessionizer.sh
+++ terminal/tmux-sessionizer.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
-session=$(find \
+session_path=$(find \
     ~ \
-    ~/code \
-    ~/code/Languages \
-    ~/dev \
+    ~/.config \
     ~/Dropbox/code \
+    ~/Dropbox/code/languages \
     ~/Dropbox/dev \
     ~/Dropbox/DropsyncFiles \
     ~/Dropbox/projects \
+    ~/repos \
     -mindepth 1 -maxdepth 1 -type d | fzf
 )
 
-session_name=$(basename "$session" | tr . _)
+# Base folder name with underscores instead of dots
+session_name=$(basename "$session_path" | tr . _)
 
-echo "session: $session"
 echo "session_name: $session_name"
+echo "session_path: $session_path"
 
 if ! tmux has-session -t "$session_name" 2>/dev/null; then
-    tmux new-session -s "$session_name" -c "$session" -d
+    tmux new-session -s "$session_name" -c "$session_path" -d
 fi
 
 tmux switch-client -t "$session_name"
