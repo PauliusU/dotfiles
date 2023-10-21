7d6085a1d8dcf5cdb259ee5614ac5d3ff094a802
PauliusU <48020370+PauliusU@users.noreply.github.com>
2023-10-21 23:24:30 +0300

    feat: small improvements in aliases and scrips

--- scripts/fs.sh
+++ scripts/fs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "**** File system ****"
-node $DOTFILES/scripts/private/dropboxIgnore.js
-python $DOTFILES/scripts/fs_dropbox_ignore_node_modules.private.py
-python $DOTFILES/scripts/fs_long_path_names_checker.private.py
+node "$DOTFILES/scripts/private/dropboxIgnore.js"
+python "$DOTFILES/scripts/fs_dropbox_ignore_node_modules.private.py"
+python "$DOTFILES/scripts/fs_long_path_names_checker.private.py"
 
 echo "**** Backup ****"
-$DOTFILES/scripts/fs-backup.sh
+"$DOTFILES/scripts/fs-backup.sh"
 
 echo "**** Software updates ****"
-$DOTFILES/scripts/updater.sh
+"$DOTFILES/scripts/updater.sh"
