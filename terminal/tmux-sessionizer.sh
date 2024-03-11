67f3d7cba89d7f3b5607d57bf6576460a2e1c6b9
PauliusU <48020370+PauliusU@users.noreply.github.com>
2024-03-11 12:29:52 +0200

    feat: add check for tmux installation and create session if it doesn't exist

--- terminal/tmux-sessionizer.sh
+++ terminal/tmux-sessionizer.sh
@@ -12,6 +12,11 @@ session_path=$(find \
     -mindepth 1 -maxdepth 1 -type d | fzf
 )
 
+if  ! command -v tmux; then
+    echo "tmux is not installed. Switching path to $session_path"
+    cd "$session_path" || exit 1
+fi
+
 # Base folder name with underscores instead of dots
 session_name=$(basename "$session_path" | tr . _)
 
@@ -19,6 +24,7 @@ echo "session_name: $session_name"
 echo "session_path: $session_path"
 
 if ! tmux has-session -t "$session_name" 2>/dev/null; then
+    echo "tmux session does not exist, create it"
     tmux new-session -s "$session_name" -c "$session_path" -d
 fi
 
