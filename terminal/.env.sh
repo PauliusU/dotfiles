157762a7cd21dec435a25a1b510ca12f6b02b36e
PauliusU <48020370+PauliusU@users.noreply.github.com>
2024-03-25 11:03:58 +0200

    refactor: extract env var setup into a separate script

---
+++ terminal/.env.sh
@@ -0,0 +1,58 @@
+# Script for setting up environment variables and PATH
+echo ".env.sh"
+
+# Default programs and settings
+export EDITOR="nvim"
+export VISUAL="code -w"
+export LANG="en_US.UTF-8"
+# Common paths
+export DOTFILES="$HOME/Dropbox/code/dotfiles"
+export SCRIPTS_PATH="$HOME/Dropbox/code/dotfiles/scripts"
+# XDG paths
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+# Home directory cleanup
+# go env -w GOPATH=$HOME/.cache/go
+export CARGO_HOME="$XDG_DATA_HOME"/cargo
+export GOPATH="$XDG_DATA_HOME"/go
+export NVM_DIR="$XDG_DATA_HOME"/nvm
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+# source "$HOME/.cargo/env" # add Rust to system PATH
+
+if [ "$(uname)" = "Darwin" ]; then
+    prepend_path() {
+        # Prepend directory to PATH
+        # Skipped if directory does not exist
+        # Argument should be /absolute/path or ~/user/path
+        if [ -d "$1" ]; then
+            # Export to sub-processes (make it inherited by child processes)
+            export PATH="$1:$PATH"
+        fi
+    }
+
+    # export BREW_HOME="$HOME/bin/homebrew"
+    export CONDA_HOME="$HOME/bin/homebrew/anaconda3"
+    export PYTHON_312_HOME="$HOME/Library/Python/3.12"
+    export VOLTA_HOME="$HOME/.config/volta"
+
+    # Order matters, first entry in PATH will take priority over later ones
+    prepend_path "$CONDA_HOME/bin"      # Conda lowest in precedence for Python
+    prepend_path "$HOME/.local/bin"     # Python packages store some bins here
+    # prepend_path "$BREW_HOME/bin"       # Use base Python instead of conda Python by default
+    prepend_path "$PYTHON_312_HOME/bin" # User packages are installed here
+    prepend_path "$VOLTA_HOME/bin"      # Use Volta Node instead of global Node by default
+
+    # Clean-up
+    unset -f prepend_path
+fi
+
+# Colorful man pages in 'less' pager
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
--- terminal/.shellrc.sh
+++ terminal/.shellrc.sh
@@ -1,62 +1,11 @@
 # Common settings for ~/.zshrc or ~/.bashrc files
 
-# -------------------------- ENV VARIABLES AND PATH  ---------------------------
-
-# Default programs and settings
-export EDITOR="nvim"
-export VISUAL="code -w"
-export LANG="en_US.UTF-8"
-# Common paths
+# Load aliases (and other custom settings)
 export DOTFILES="$HOME/Dropbox/code/dotfiles"
-export SCRIPTS_PATH="$HOME/Dropbox/code/dotfiles/scripts"
-# XDG paths
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state"
-# Home directory cleanup
-# go env -w GOPATH=$HOME/.cache/go
-export CARGO_HOME="$XDG_DATA_HOME"/cargo
-export GOPATH="$XDG_DATA_HOME"/go
-export NVM_DIR="$XDG_DATA_HOME"/nvm
-export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
-# source "$HOME/.cargo/env" # add Rust to system PATH
-
-if [ "$(uname)" = "Darwin" ]; then
-    prepend_path() {
-        # Prepend directory to PATH
-        # Skipped if directory does not exist
-        # Argument should be /absolute/path or ~/user/path
-        if [ -d "$1" ]; then
-            # Export to sub-processes (make it inherited by child processes)
-            export PATH="$1:$PATH"
-        fi
-    }
-
-    # export BREW_HOME="$HOME/bin/homebrew"
-    export CONDA_HOME="$HOME/bin/homebrew/anaconda3"
-    export PYTHON_312_HOME="$HOME/Library/Python/3.12"
-    export VOLTA_HOME="$HOME/.config/volta"
-
-    # Order matters, first entry in PATH will take priority over later ones
-    prepend_path "$CONDA_HOME/bin"      # Conda lowest in precedence for Python
-    prepend_path "$HOME/.local/bin"     # Python packages store some bins here
-    # prepend_path "$BREW_HOME/bin"       # Use base Python instead of conda Python by default
-    prepend_path "$PYTHON_312_HOME/bin" # User packages are installed here
-    prepend_path "$VOLTA_HOME/bin"      # Use Volta Node instead of global Node by default
-
-    # Clean-up
-    unset -f prepend_path
-fi
-
-# Colorful man pages in 'less' pager
-export LESS_TERMCAP_mb=$'\e[1;32m'
-export LESS_TERMCAP_md=$'\e[1;32m'
-export LESS_TERMCAP_me=$'\e[0m'
-export LESS_TERMCAP_se=$'\e[0m'
-export LESS_TERMCAP_so=$'\e[01;33m'
-export LESS_TERMCAP_ue=$'\e[0m'
-export LESS_TERMCAP_us=$'\e[1;4;31m'
+source "$DOTFILES/terminal/.env.sh"
+source "$DOTFILES/terminal/.aliases.sh"
+source "$DOTFILES/terminal/.functions.sh"
+source "$DOTFILES/terminal/.shellrc.private.sh"
 
 # ----------------------------------- TMUX -------------------------------------
 if [ -n "$ZSH_VERSION" ]; then # zsh
@@ -151,14 +100,9 @@ if [ -n "$ZSH_VERSION" ]; then
     #  Zsh Line Editor (ZLE)
     zle -N _zsh_fancy_ctrl_z
     bindkey "^Z" _zsh_fancy_ctrl_z
+
+    eval "$(zoxide init zsh)" # enable zoxide for zsh
 fi
-
-eval "$(zoxide init zsh)" # enable zoxide for zsh
-
-# Load aliases (and other custom settings)
-source "$DOTFILES/terminal/.aliases.sh"
-source "$DOTFILES/terminal/.functions.sh"
-source "$DOTFILES/terminal/.shellrc.private.sh"
 
 get_os # Defined in .functions.sh
 echo ".shellrc.sh for $OSFOUND loaded"
