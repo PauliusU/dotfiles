28ce396de24caf22022a0d1a87dd4e7e1dce814a
PauliusU <48020370+PauliusU@users.noreply.github.com>
2024-03-25 11:04:29 +0200

    fix(nvim): add missing dependency for nvim-dap-ui

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
         -- Creates a beautiful debugger UI
         'rcarriga/nvim-dap-ui',
+        "nvim-neotest/nvim-nio", -- dependency for nvim-dap-ui
 
         -- Installs the debug adapters for you
         'williamboman/mason.nvim',
