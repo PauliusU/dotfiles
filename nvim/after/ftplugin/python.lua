301895e175acbc36de82348cdcab13f56a5a66b4
PauliusU <48020370+PauliusU@users.noreply.github.com>
2025-03-04 11:55:21 +0200

    format(nvim): filetype plugins

---
+++ nvim/after/ftplugin/python.lua
@@ -0,0 +1,6 @@
+print('🐍 python.lua')
+
+vim.bo.tabstop = 4      -- size of a hard tabstop (ts).
+vim.bo.shiftwidth = 4   -- size of an indentation (sw).
+vim.bo.expandtab = true -- always uses spaces instead of tab characters (et).
+vim.bo.softtabstop = 3  -- number of spaces a <Tab> counts for. When 0, feature is off (sts).
