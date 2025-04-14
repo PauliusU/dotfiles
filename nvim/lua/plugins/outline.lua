a1d65f4e22f4fc9c657fb7de38a7b5faae46d301
PauliusU <48020370+PauliusU@users.noreply.github.com>
2025-04-14 14:40:46 +0300

    feat(misc): add configuration for ghostty and kitty, enhance date-time espanso matches, update nvim plugins, and improve updater script

--- nvim/lua/plugins/outline.lua
+++ nvim/lua/plugins/outline.lua
@@ -1,11 +1,15 @@
 return {
     "hedyhli/outline.nvim",
+    desc = "A sidebar with a tree-like outline of symbols from your code, powered by LSP",
     lazy = true,
     cmd = { "Outline", "OutlineOpen" },
     keys = {
         { "go", "<cmd>Outline<CR>", desc = "Toggle outline" },
     },
     opts = {
-        -- Your setup opts here
+        symbol_folding = {
+            -- Depth past which nodes will be folded by default. Set to false to unfold all on open.
+            autofold_depth = 3,
+        },
     },
 }
