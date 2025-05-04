4b65481d56c2d580600a0ed57195f32cf000d5e2
PauliusU <48020370+PauliusU@users.noreply.github.com>
2025-05-04 19:36:36 +0300

    feat(nvim-tree): implement natural sorting for file names in the tree view

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -15,15 +15,64 @@ return {
         -- Set termguicolors to enable highlight groups
         vim.opt.termguicolors = true
 
+        -- Ref: https://github.com/nvim-tree/nvim-tree.lua/wiki/Recipes#sorting-files-naturally-respecting-numbers-within-files-names
+        local function natural_cmp(left, right)
+            left = left.name:lower()
+            right = right.name:lower()
+
+            if left == right then
+                return false
+            end
+
+            for i = 1, math.max(string.len(left), string.len(right)), 1 do
+                local l = string.sub(left, i, -1)
+                local r = string.sub(right, i, -1)
+
+                if type(tonumber(string.sub(l, 1, 1))) == "number" and type(tonumber(string.sub(r, 1, 1))) == "number" then
+                    local l_number = tonumber(string.match(l, "^[0-9]+"))
+                    local r_number = tonumber(string.match(r, "^[0-9]+"))
+
+                    if l_number ~= r_number then
+                        return l_number < r_number
+                    end
+                elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
+                    return l < r
+                end
+            end
+        end
+
         -- Default options: https://github.com/nvim-tree/nvim-tree.lua/blob/aaee4cd896b74f85a81bed7eef2db7869960c4d0/lua/nvim-tree.lua#L361
         require("nvim-tree").setup({
+            sort_by = function(nodes)
+                table.sort(nodes, natural_cmp)
+            end,
             view = {
                 side = "right", -- Set the tree to appear on the right-hand side
-                width = 40, -- Make panel broader. Default: 30
+                width = 40,     -- Make panel broader. Default: 30
             },
             filters = {
-                git_ignored = false,   -- shows git ignored files in the tree
-                custom = { '^.git$' }, -- hides .git folder in the tree
+                git_ignored = false,   -- Shows git ignored files in the tree
+                custom = { '^.git$' }, -- Hides .git folder in the tree
+            },
+            renderer = {
+                highlight_git = true, -- Highlight git status
+                -- highlight_opened_files = "all",                          -- Highlight opened files
+                -- root_folder_modifier = ":t",                             -- Show only the file name in the tree
+                -- full_name = false,                                       -- Show full path in the tree
+                -- group_empty = true,                                      -- Group empty folders
+                -- special_files = { "README.md", "Makefile", "MAKEFILE" }, -- Special files to be highlighted
+                indent_markers = {
+                    enable = true,
+                },
+                -- icons = {
+                --     git_placement = "signcolumn",
+                --     show = {
+                --         file = true,
+                --         folder = false,
+                --         folder_arrow = false,
+                --         git = true,
+                --     },
+                -- },
             },
         })
     end
